aa8ec01123a1c8d59f9b37fe8456337620ad4b6a
aka_Vohid <vaxidjonbaratov@gmail.com>
2024-01-19 09:52:38 +0500

    Initial commit

---
+++ src/uz/pdp/Main.java
@@ -0,0 +1,10 @@
+package uz.pdp;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("asSAFADFA6555555555555555555c");
+    }
+}
